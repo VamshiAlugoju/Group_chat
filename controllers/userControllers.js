897cdfda4d9be688a53299fae194f2f7017c4f36
vamshi <alugojuvamshi@gmail.com>
2023-05-06 20:44:40 +0530

    jwt added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,13 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const User = require("../models/User")
+
+function generatetoken(userId)
+{
+  return  jwt.sign(userId,process.env.TOKEN)
+}
 
 exports.SignUp = async (req,res)=>{
      
@@ -8,16 +15,20 @@ exports.SignUp = async (req,res)=>{
   const {Name,Password,Email,Number}= req.body;
 
   try{
-      bcrypt.hash(Password,10, async(err,hash)=>{
-    
-        if(err)
-          throw new Error("internal error");
-        
-         await User.create({Name,Password:hash,Email,Number});
-        
-         res.status(200).json({message:"SignUp successfull"});
-    
-      })
+     let user = await User.findOne({where:{Email}})
+     if(user)
+       res.json({message:"user already exist please Login"})
+     else{
+         bcrypt.hash(Password,10, async(err,hash)=>{
+       
+           if(err)
+             throw new Error("internal error");
+           
+            await User.create({Name,Password:hash,Email,Number});
+           
+            res.status(200).json({message:"SignUp successfull"});
+         })
+     }
   }
   catch(err)
   {
@@ -26,7 +37,27 @@ exports.SignUp = async (req,res)=>{
 
 }
 
-exports.Login = (req,res)=>{
+exports.Login = async (req,res)=>{
 
+     try{
+        const {Email,Password} = req.body;
+        let user =  await User.findOne({where:{Email}});
+        if(!user){
+          return  res.status(204).json("cannot find user with Email id");
+        }
+        bcrypt.compare(Password,user.Password,(err,result)=>{
+            if(err)
+              throw new Error("internal error");
+              console.log(result)
+            if(result)
+              res.status(201).json({success:true,token:generatetoken(user.id)});
+            else
+              res.status(201).json({success:false})
+        })
+     }
+     catch(err)
+     {
+       res.status(400).json(err);
+     }
 
 }
